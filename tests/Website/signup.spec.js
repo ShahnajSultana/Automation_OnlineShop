d0a19efba23590ff7b7517c2197832cccd718da7
ShahnajSultana <44563670+ShahnajSultana@users.noreply.github.com>
2024-06-26 00:22:26 +0200

    Update signup.spec.js

--- tests/Website/signup.spec.js
+++ tests/Website/signup.spec.js
@@ -5,5 +5,5 @@ test("test", async ({ page }) => {
   const SignUp = new SignUpPage(page);
 
   await SignUp.gotoSignUpPage();
-  await SignUp.signup("shahnajqa32@yopmail.com", "Test", "Shahnaj", "123456");
+  await SignUp.signup("shahnajqa35@yopmail.com", "Test", "Shahnaj", "123456");
 });
